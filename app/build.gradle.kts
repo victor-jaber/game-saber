7087ac4e4f67574734e0e6b6da6d394de5c98a36
Victor Jaber <victorjaber19@gmail.com>
2026-03-05 11:05:45 -0300

    chore: bump app version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.victorjaber.gamejaber"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
